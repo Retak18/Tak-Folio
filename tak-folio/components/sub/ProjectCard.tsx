d1d6e0479b38992482c46c342937318b3dacba3f
Tarek <tarek_lamarti@live.fr>
2025-03-24 13:51:39 +0100

    modif d'affichage

--- tak-folio/components/sub/ProjectCard.tsx
+++ tak-folio/components/sub/ProjectCard.tsx
@@ -23,14 +23,14 @@ const ProjectCard = ({ src, title, description, technologies }: Props) => {
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
         <h5  className="mt-2 cursor cursor-pointer text-blue-300">Technologies :</h5>
-        <h5  className="mt-2 cursor cursor-pointer text-blue-300">{technologies.map((tech, i) => (
+        <div className="flex flex-wrap gap-2 mt-4">{technologies.map((tech, i) => (
                   <span
                     key={i}
                     className="px-2 py-1 text-xs bg-background rounded-full"
                   >
                     {tech}
                   </span>
-                ))}</h5>
+                ))}</div>
       </div>
     </div>
   );
