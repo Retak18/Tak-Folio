ad4610e2acf14e1c4f486e67f2ba28cda39b63cb
retak18 <tarek_lamarti@live.fr>
2025-07-28 17:34:28 +0200

    Refactor code structure for improved readability and maintainability

--- tak-folio/app/layout.tsx
+++ tak-folio/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
@@ -25,6 +26,7 @@ export default function RootLayout({
         <StarsCanvas />
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
